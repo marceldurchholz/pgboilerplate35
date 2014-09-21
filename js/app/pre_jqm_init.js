0390a78f5ddefa00ace6f2f68f1aa004e9b80ffd
DigitalVerve <info@digitalverve.de>
2014-09-21 10:16:08 +0200

    21.09.2014_10:16:08,25

--- js/app/pre_jqm_init.js
+++ js/app/pre_jqm_init.js
@@ -12,38 +12,37 @@ $( document ).bind( "mobileinit", function() {
 	$.mobile.allowCrossDomainPages = true;
 	
 	document.addEventListener('deviceready', function () {
-		$(document.body).height(window.innerHeight);
+		//$(document.body).height(window.innerHeight);
 		// document.body.height(window.innerHeight);
-		alert('a');
+		//alert('a');
 	}, false);
+	
 	if (isMobile.any()) {
-		// change native things...
-		// alert('mobile device');
 		StatusBar.overlaysWebView(false);
 		StatusBar.styleLightContent();
 		StatusBar.show();
-
 	} else  {
-		// alert('desktop device');
 	}
 
 	/* iOS */
-    // $('textarea').unbind('focusout');
-    $('textarea').unbind('blur');
-	$("textarea").on('blur', 'textarea', function() {
-		console.log('blur');
-		$(window).scrollTop(10);
-		$(window).scrollTop(0);
-		var footer = $("#footer_message");
-		console.log(footer);
-		footer.css({ "top": footer.position().top, "bottom": "auto"});
-	});
-
+    $('textarea').unbind('focusin');
+    $(document).on('focusin', 'textarea', function() {
+		console.log('focusin');
+        setTimeout(function() {
+			console.log('focusin B');
+			window.scrollTo(document.body.scrollLeft, document.body.scrollTop);
+			var footer = $(".textmsg");
+			console.log(footer.position().top);
+            footer.css({ "height":"120px", "position":"fixed","border":"5px solid red", "top":"auto", "bottom": "60px"});
+        }, 1000);
+		// $(window).scrollTop(0);
+    });
+	
     $('textarea').unbind('focusout');
     $(document).on('focusout', 'textarea', function() {
 		console.log('focusout');
-		$(window).scrollTop(10);
 		$(window).scrollTop(0);
+		/*
 		var footer = $("#footer_message");
 		footer.css({ "top": footer.position().top, "bottom": "auto"});
         setTimeout(function() {
@@ -53,24 +52,14 @@ $( document ).bind( "mobileinit", function() {
 
 			footer.css({ "top": footer.position().top, "bottom": "auto"});
         }, 500);
+		*/
     });
 
-    $('textarea').unbind('focusin');
-    $(document).on('focusin', 'textarea', function() {
-		console.log('focusin');
-		$(window).scrollTop(10);
+    $('textarea').unbind('blur');
+	$("textarea").on('blur', 'textarea', function() {
+		console.log('blur');
 		$(window).scrollTop(0);
-		var footer = $("#footer_message");
-		footer.css({ "top": footer.position().top, "bottom": "auto"});
-        setTimeout(function() {
-			console.log('focusin B');
-            window.scrollTo(document.body.scrollLeft, document.body.scrollTop);
-			var footer = $("#footer_message");
-
-			footer.css({ "top": footer.position().top, "bottom": "auto"});
-        }, 500);
-    });
-	
+	});
 
     $.mobile.jqmRouter={
     	firstMatchOnly: true
