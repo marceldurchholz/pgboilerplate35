d54901f2567768b64fdb69adb281c6388de1a90c
DigitalVerve <info@digitalverve.de>
2014-09-22 22:38:25 +0200

    22.09.2014_22:38:25,04

--- js/app/pre_jqm_init.js
+++ js/app/pre_jqm_init.js
@@ -38,7 +38,7 @@ $( document ).bind( "mobileinit", function() {
 				console.log(e.target);
 				console.log('setTimeout( function() {...');
 				// console.log($('#header_message').offset().top);
-				$(e.target).css({'position':'fixed','bottom':'0px','left':'0px','z-index':'9999'});
+				// $(e.target).css({'position':'fixed','bottom':'0px','left':'0px','z-index':'9999'});
 				// $.mobile.silentScroll($('#header_message').offset().top);
 				// console.log($(document.body).html());
 				console.log($('#page_message').html());
